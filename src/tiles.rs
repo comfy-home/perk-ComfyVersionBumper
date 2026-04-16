4dc0271f3e8997f629f0c80b3514b90e3523e4bf
Tom @ ComfyHome® <dev@comfyhome.io>
2026-04-16 18:26:09 +0100

    @.feat: Ignore: *this *and this; @.feat: Ignore: *this too *and this too

--- src/tiles.rs
+++ src/tiles.rs
@@ -1,3 +1,10 @@
+// Copyright © 2026 ComfyHome™
+// All rights reserved.
+//
+// Licensed under the ComfyVersionBumper License v1.2
+//
+// For details, see the LICENSE file in the repository root.
+
 use ratatui::{
 	Frame,
 	layout::{Alignment, Rect},
