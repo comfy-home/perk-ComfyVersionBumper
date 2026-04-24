bafc6288a933965d4f52abf7e511579c6e1ded90
Tom @ ComfyHome® <dev@comfyhome.io>
2026-04-24 13:06:15 +0100

    enh(Changelog Ordering Logic): Ordering now follows new rules: *ordering within one group changed to oldest→latest **this should help end-users with context understanding *Ordering by category/modifier is now: **Release notes **`!` Breaking Change **`@.` New announcement **`@` New in... **`...()` "Changed in", eg `enh(Login Flow)` **`...` "Standard", eg `fix`, or `feat`

--- src/git_pr.rs
+++ src/git_pr.rs
@@ -1,10 +1,8 @@
 // Copyright © 2026 ComfyHome™
 // All rights reserved.
-///
 // Licensed under the ComfyGit License v1.2
 //
 // For details, see the LICENSE file in the repository root.
-
 use std::{
     collections::HashSet,
     io::{self, Write},
